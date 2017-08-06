bb0a3c77b2bb286f6ef653632635e97a02663034
Steven Hankin <steven.hankin@gmail.com>
2017-08-06 20:26:25 +0100

    Add user to database

--- items.py
+++ items.py
@@ -5,10 +5,21 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
 class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key = True)
     name =Column(String(80), nullable = False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
     #address = Column(String(250))
     #city = Column(String(80))
     #state = Column(String(20))
@@ -24,6 +35,8 @@ class Item(Base):
     #picture = Column(String)
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
     #weight = Column(Numeric(10))
 
 
