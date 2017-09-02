8250d6ac985ebf3c9532b4199641d9b118f3e011
Steven Hankin <steven.hankin@gmail.com>
2017-09-02 15:41:09 +0100

    Enable all CRUD functionality Also added flash messages

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, g, abort
+from flask import Flask, render_template, request, redirect, url_for, g, abort, flash
 from sqlalchemy import asc, desc, text
 from sqlalchemy.orm.exc import NoResultFound
 from database_setup import Base, User, Category, Item, session
@@ -25,19 +25,13 @@ def show_homepage():
     Shows all the available categories and items
     :return:
     """
-    print "** LOGIN_SESSION is"
-    print login_session
-
     max_items = 5
     all_categories = session.query(Category).order_by(asc(Category.name)).all()
-    # if 'username' not in login_session:
-    #     return render_template('public_categories.html', categories=categories)
-    # else:
     s = "select item.name as item_name, category.name as category_name, " \
         " category.id as category_id " \
         " from item" \
         " join category on (item.category_id = category.id)" \
-        " order by item.time_created DESC" \
+        " order by ifnull(item.time_updated, item.time_created) DESC" \
         " limit " + str(max_items)
     latest_items = session.execute(s).fetchall()
     return render_template('categories_latest.html',
@@ -55,7 +49,6 @@ def show_category_items(category_id):
     :param category_id:
     :return:
     """
-    print "category_id " + str(category_id)
     all_categories = session.query(Category).order_by(asc(Category.name)).all()
     category = session.query(Category).filter(Category.id == category_id).first()
     items = session.query(Item).filter(Item.category_id == category_id)
@@ -72,49 +65,97 @@ def show_category_items(category_id):
 def show_item_details(category_id, item_id):
     """
     Displays full description of an item
-    :param category_id:
-    :param item_id:
-    :return:
     """
-    print "** ITEM LOGIN_SESSION is"
-    print login_session
-    print "item_id " + str(item_id)
-    # all_categories = session.query(Category).order_by(asc(Category.name)).all()
-    # category = session.query(Category).filter(Category.id == category_id).first()
     item = session.query(Item, User).join(User).filter(Item.id == item_id).one()
-    # item_count = items.count()
-    print item.Item.description
     return render_template('item_details.html', item=item, login_session=login_session)
+
+
+@app.route('/items/<int:item_id>/delete', methods=['POST', 'GET'])
+def delete_item_details(item_id):
+    item = asset_user_is_creator(item_id)
+
+    item_name = item.Item.name
+
+    if request.method == 'GET':
+        return render_template('item_delete_confirm.html', item_name=item_name, item_id=item_id,
+                               login_session=login_session)
+    else:
+        delete_item = session.query(Item).filter(Item.id == item_id).one()
+        session.delete(delete_item)
+        session.commit()
+        flash(item_name + " deleted")
+        return redirect(url_for('show_homepage'))
+
+
+def asset_user_is_creator(item_id):
+    # User must be logged in for GET and POST
+    if not login_session.has_key('userid'):
+        abort(403)
+    item = session.query(Item, User).join(User).filter(Item.id == item_id).first()
+    # For existing items, user must be item creator
+    if item and item.Item.user_id != login_session['userid']:
+        abort(403)
+    return item
 
 
 @app.route('/items/<int:item_id>/edit', methods=['POST', 'GET'])
 def edit_item_details(item_id):
     """
-    Edit item details IF user originally created the item
+    item_id >= 1  =>  UPDATE item  (if user is original creator)
+    item_id == 0  =>  CREATE item
+    """
+
+    item = asset_user_is_creator(item_id)
+
+    if request.method == 'GET':
+        categories = session.query(Category).order_by(asc(Category.name)).all()
+        return display_item(categories, item, item_id)
+    else:
+        return save_item(item, item_id)
+
+
+def save_item(item, item_id):
+    """
+    For updating an existing item
+    or creating a new item
+    :param item:
     :param item_id:
     :return:
     """
+    # User is modifying an EXISTING item in the database
+    if item_id > 0:
+        item.Item.name = request.form['title']
+        item.Item.description = request.form['description']
+        item.Item.category_id = request.form['category']
+        session.commit()
+        flash("Updated " + item.Item.name)
+        return render_template('item_details.html', item=item, login_session=login_session)
 
-    print "Login session is..."
-    print login_session
+    # User is creating a NEW item
+    else:
+        new_item = Item(name=request.form.get('title'), description=request.form['description'],
+                        category_id=request.form['category'],
+                        user_id=login_session['userid'])
+        session.add(new_item)
+        session.commit()
+        flash("Created " + new_item.name)
+        item = session.query(Item, User).join(User).filter(Item.id == new_item.id).first()
+        return render_template('item_details.html', item=item, login_session=login_session)
 
-    # User must be logged in for GET and POST
-    if not login_session.has_key('userid'):
-        abort(403)
 
-    item = session.query(Item, User).join(User).filter(Item.id == item_id).one()
-
-    print "item creator "+str(item.Item.user_id)+", current user "+str(login_session['userid'])
-    # User must be item creator
-    if item.Item.user_id != login_session['userid']:
-        abort(403)
-
-    categories = session.query(Category).order_by(asc(Category.name)).all()
-
-    if request.method == 'GET':
-        return render_template('item_details_edit.html', item=item, categories=categories, login_session=login_session)
-    if request.method == 'POST':
-        return 'Ok!!'
+def display_item(categories, item, item_id):
+    if item:
+        # Item already exists - display on page
+        return render_template('item_edit.html', item_id=item_id, item_name=item.Item.name,
+                               item_description=item.Item.description, item_category=item.Item.category,
+                               item_category_id=item.Item.category_id, categories=categories,
+                               login_session=login_session)
+    else:
+        # Default fields for creating a new item
+        return render_template('item_edit.html', item_id=0, item_name="",
+                               item_description="", item_category="",
+                               item_category_id=0, categories=categories,
+                               login_session=login_session)
 
 
 def redirect_dest(fallback):
@@ -129,9 +170,9 @@ def redirect_dest(fallback):
 @app.route('/logout')
 def logout():
     login_session.clear()
-    nextRedirect = request.args.get('next')
-    print "next is " + nextRedirect
-    return redirect(nextRedirect)
+    next_redirect = request.args.get('next')
+    flash('You have been logged out')
+    return redirect(next_redirect)
 
 
 @app.route('/login')
@@ -142,14 +183,12 @@ def login_redirect():
     """
     nextRedirect = request.args.get('next')
     access_token = request.args.get('access_token')
-    print "access token is " + access_token
     d = amazon_authorization(access_token)
     # # State token to prevent CSRF
     # state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
     # login_session['state'] = state
     # Find user in database by email or create new record
     user = session.query(User).filter(User.email == d['email']).first()
-    print user
     if user is None:
         m = hashlib.md5()
         m.update(d['email'])
@@ -161,6 +200,8 @@ def login_redirect():
     login_session['userid'] = user.id
     login_session['picture'] = user.picture
     login_session['name'] = user.name
+
+    flash('You were successfully logged in')
 
     return redirect_dest(nextRedirect)
 
@@ -192,5 +233,4 @@ def amazon_authorization(access_token):
     c.setopt(pycurl.WRITEFUNCTION, b.write)
     c.perform()
     d = json.loads(b.getvalue())
-    print "%s %s %s" % (d['name'], d['email'], d['user_id'])
     return d
